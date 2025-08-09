d9d68eb27e26bad0c841c367ac929cd4bbf8e2bc
Kit Daniel Lim <kitdaniellim@gmail.com>
2025-08-09 09:01:59 +0800

    Fix build errors

--- src/images/catering/index.ts
+++ src/images/catering/index.ts
@@ -1,8 +1,8 @@
 import catering1 from "./catering_1.jpg";
-import catering2 from "./catering_2.jpg";
-import catering3 from "./catering_3.jpg";
-import catering4 from "./catering_4.jpg";
-import catering5 from "./catering_5.jpg";
-import catering6 from "./catering_6.jpg";
+import catering2 from "./catering_2.JPG";
+import catering3 from "./catering_3.JPG";
+import catering4 from "./catering_4.JPG";
+import catering5 from "./catering_5.JPG";
+import catering6 from "./catering_6.JPG";
 
 export { catering1, catering2, catering3, catering4, catering5, catering6 };
--- src/images/services/index.ts
+++ src/images/services/index.ts
@@ -2,6 +2,6 @@ import service1 from "./services_1.jpg";
 import service2 from "./services_2.jpg";
 import service3 from "./services_3.jpg";
 import service4 from "./services_4.jpg";
-import service5 from "./services_5.jpg";
+import service5 from "./services_5.png";
 
 export { service1, service2, service3, service4, service5 };
