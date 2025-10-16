a19f08a48657cda72888959ac97032ffa5be8cce
Kit Daniel Lim <kitdaniellim@gmail.com>
2025-10-17 04:50:23 +0800

    Removed StrictMode react

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { PrimeReactProvider } from 'primereact/api';
 import 'primeicons/primeicons.css';
@@ -6,9 +5,7 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <PrimeReactProvider>
       <App />
     </PrimeReactProvider>
-  </StrictMode>
 );
